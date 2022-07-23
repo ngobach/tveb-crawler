d2a7cf3d258362e7ae928cadbb087af91508c293
Bach Ngo <thanbaiks@gmail.com>
2022-07-23 23:31:54 +0700

    feat: wip

--- src/crawler.js
+++ src/crawler.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import fs from 'fs/promises';
+import puppeteer from 'puppeteer';
 import { fetchAndLoad } from './utils';
 
 const TargetBase = 'https://nhasachmienphi.com';
@@ -7,21 +8,39 @@ const TargetBase = 'https://nhasachmienphi.com';
 const DataDir = path.join(process.cwd(), 'data');
 
 class Crawler {
+  /**
+   * @type {puppeteer.Browser}
+   */
+  #browser = null;
+
   async startCrawling () {
-    // const cates = await this.#crawlCates();
-    // this.#fileWriteJSON('categories.json', cates);
-    console.log('Done crawling categories');
+    if (!await this.#fileExists('categories.json')) {
+      this.#fileWriteJSON('categories.json', await this.#crawlCates());
+      console.log('Done crawling categories');
+    }
+    // const categories = this.#fileReadJSON('categories.json');
 
-    // const indexes = await this.#fileReadJSON('indexes.json');
-    const indexes = await this.#crawlIndexes();
-    this.#fileWriteJSON('indexes.json', indexes);
-    console.log('Done crawling indexes');
+    if (!await this.#fileExists('indexes.json')) {
+      this.#fileWriteJSON('indexes.json', await this.#crawlIndexes());
+      console.log('Done crawling indexes');
+    }
+    const indexes = await this.#fileReadJSON('indexes.json');
 
-    // const books = await this.#crawlBooks(indexes);
-    // this.#fileWriteJSON('books.json', books);
-    const books = await this.#fileReadJSON('books.json');
-    console.log('Done crawling books');
-    console.log(books);
+    if (!await this.#fileExists('books_raw.json')) {
+      this.#fileWriteJSON('books_raw.json', await this.#crawlBooks(indexes));
+      console.log('Done crawling books');
+    }
+    const rawBooks = await this.#fileReadJSON('books_raw.json');
+
+    this.#browser = await puppeteer.launch();
+
+    // if (!await this.#fileExists('books.json')) {
+    // this.#fileWriteJSON('books.json', await this.#ripBooks(rawBooks.slice(0, 2)));
+    // console.log('Done ripping books');
+    // }
+
+    await this.#ripUrl('https://nhasachmienphi.com/doc-online/truyen-tranh-loc-dinh-ky-326474', 'foo-bar', 'foo-bar');
+    await this.#browser.close();
   }
 
   async #crawlCates () {
@@ -32,7 +51,7 @@ class Crawler {
 
       return {
         title: anchorElement.text(),
-        slug: anchorElement.attr('href').replace(/^.*\/(.*)$/i, '$1')
+        slug: anchorElement.attr('href').replace(/^.*\/(.*)$/i, '$1'),
       };
     });
 
@@ -61,7 +80,6 @@ class Crawler {
         indexes.push(...elems.map(el => ({
           slug: $(el).find('a').attr('href').replace(/^.*\/(.*)\.html$/i, '$1'),
           title: $(el).find('h4').text(),
-          // thumbnailUrl: this.#normalizeUrl($(el).find('img.medium_thum').attr('src'))
         })));
       }
     }));
@@ -89,7 +107,7 @@ class Crawler {
           title: $('h1.tblue').text(),
           category: $('a.tblue').attr('href').replace(/^.*\/(.*)$/, '$1'),
           thumbnailUrl: this.#normalizeUrl($('.content_page img').attr('src')),
-          resources: {}
+          resources: {},
         };
 
         const $btns = $('.button');
@@ -114,8 +132,47 @@ class Crawler {
     return books;
   }
 
-  async #fileWriteBin (filePath, data) {
+  async #ripBooks (rawBooks) {
+    const workerCount = 2;
+    const jobs = Object.entries(rawBooks);
+    const books = { length: 0 };
 
+    await Promise.all(Array.from({ length: workerCount }).map(async () => {
+      const [idx, rawBook] = jobs.splice(0, 1)[0] ?? [];
+      if (idx === undefined) {
+        console.log('Worker exiting...');
+      }
+
+      console.log(rawBook);
+    }));
+
+    return Array.from(books);
+  }
+
+  async #ripUrl (url, outputPath, basename) {
+    if (url.includes('/readfile-online')) {
+      // Skip theses
+      return null;
+    } else if (url.includes('/doc-online')) {
+      // Convert to PDF
+      // const response = await fetch(url);
+      // // const html = await response.text();
+      const page = await this.#browser.newPage();
+      await page.goto(url, { waitUntil: 'load' });
+      await page.setContent((await page.$eval('.content_p', e => e.innerHTML)));
+      await this.#fileWriteBin(path.join(outputPath, `${basename}_online.pdf`), await page.createPDFStream());
+      await page.close();
+    } else {
+      const ext = url.match(/^.*\.(.{1,4})$/)?.[1] ?? `_${Math.random().toString(36).substring(2)}.bin`;
+      const response = await fetch(url);
+      await this.#fileWriteBin(path.join(outputPath, `${basename}.${ext}`), response.body);
+    }
+  }
+
+  async #fileWriteBin (filePath, data) {
+    const actualPath = path.join(DataDir, filePath);
+    await fs.mkdir(path.dirname(actualPath), { recursive: true });
+    await fs.writeFile(actualPath, data);
   }
 
   async #fileWriteJSON (filePath, data) {
@@ -128,6 +185,16 @@ class Crawler {
     const actualPath = path.join(DataDir, filePath);
 
     return JSON.parse(await fs.readFile(actualPath));
+  }
+
+  async #fileExists (filePath) {
+    const actualPath = path.join(DataDir, filePath);
+
+    try {
+      return (await fs.stat(actualPath)).isFile();
+    } catch (error) {
+      return false;
+    }
   }
 
   #normalizeUrl (rawUrl) {
